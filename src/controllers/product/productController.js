fe7c3eac36f2396ca4657c8e0a03c756a71d5929
ANH Nguyen Tri Hoang (VTI.D8) <anh.nguyentrihoang@vti.com.vn>
2023-11-10 17:38:51 +0700

    update api

--- src/controllers/product/productController.js
+++ src/controllers/product/productController.js
@@ -6,42 +6,84 @@ const oracledb = require('oracledb');
 
 const productRoute = express.Router();
 
+function getQueryString(params, logicOnly) {
+  let where = '';
+  let page = '';
+  let sort = '';
+  Object.keys(params).forEach((t) => {
+    const type = paramsKey.find((k) => {
+      return k.keys.includes(t);
+    })?.type;
+    switch (type) {
+      case 'logic':
+        const logic = `pd.id in (select product_detail_id from product_category where ${
+          t.includes('name')
+            ? `${t.replace(
+                '_name',
+                '_id',
+              )} in (SELECT id FROM categories where name like '%${
+                params[t]
+              }%'))`
+            : Array.isArray(params[t])
+            ? `${t} in (${params[t]}))`
+            : `${t}  = ${params[t]}) `
+        }`;
+        if (where.length === 0) {
+          where += `where ${logic} `;
+        } else {
+          where += `and ${logic} `;
+        }
+        break;
+      case 'page':
+        if (t === 'limit') {
+          page += `FETCH NEXT ${params[t]} ROWS ONLY `;
+        } else {
+          page = `OFFSET ${params[t]} ROWS`.concat(' ', page);
+        }
+        break;
+      case 'sort':
+        if (t === 'order_by') {
+          sort += `ORDER BY ${params[t]} `;
+        } else {
+          sort += params[t] + ', pd.create_Date';
+        }
+        break;
+      default:
+        break;
+    }
+  });
+  if (page.length === 0) {
+    page = `FETCH NEXT 28 ROWS ONLY `;
+  } else if (sort.length === 0) {
+    sort = `ORDER BY CREATE_DATE DESC`;
+  }
+  if (logicOnly) return where;
+  return where + sort + ' ' + page;
+}
+
+paramsKey = [
+  {
+    keys: ['category_id', 'brand_id', 'type_id', 'feature_id', 'category_name'],
+    type: 'logic',
+  },
+  {
+    keys: ['limit', 'offset'],
+    type: 'page',
+  },
+  {
+    keys: ['order_by', 'sort_by'],
+    type: 'sort',
+  },
+];
+
 productRoute.get('/products', (req, res) => {
   db.connect().then(async (connect) => {
-    const where = req.query.category_name
-      ? `in (SELECT id FROM categories where name like '%${req.query.category_name}%')`
-      : req.query.category_id
-      ? `= ${req.query.category_id} `
-      : '';
-    const logic =
-      (req.query.category_id || req.query.category_name
-        ? `where pd.id in (select product_detail_id from product_category where category_id ${where}) `
-        : '') +
-      (req.query.brand_id ? `and pd.brand_id = ${req.query.brand_id}` : '');
-    const pageLimit = `FETCH NEXT ${
-      req.query.limit ? req.query.limit : 10
-    } ROWS ONLY `;
-    const pageOffset =
-      ' ' +
-      (req.query.offset && req.query.offset > 0
-        ? `OFFSET ${req.query.offset} ROWS `
-        : '');
-    const sort_by =
-      (req.query.sort_by
-        ? `ORDER BY ${req.query.sort_by.toUpperCase()}`
-        : 'ORDER BY CREATE_DATE') +
-      ` ${req.query.order_by ? req.query.order_by : 'DESC'}`;
     const query =
-      `SELECT pd.* FROM product_detail pd ` +
-      logic +
-      sort_by +
-      pageOffset +
-      pageLimit;
-
+      `SELECT pd.* FROM product_detail pd ` + getQueryString(req.query);
     const lengthQuery =
-      `SELECT count(pd.id)  as length
-    FROM product_detail  pd
-     ` + logic;
+      `SELECT count(pd.id) as length FROM product_detail pd ` +
+      getQueryString(req.query, true);
+    console.log(query);
     const length = await (
       await connect.execute(lengthQuery, {})
     ).rows[0].LENGTH;
