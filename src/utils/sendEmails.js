7739d346ae20dd7c468e1d0f9038de5c657efc8e
ANH Nguyen Tri Hoang (VTI.D8) <anh.nguyentrihoang@vti.com.vn>
2024-03-29 17:31:32 +0700

    add reset password

---
+++ src/utils/sendEmails.js
@@ -0,0 +1,29 @@
+const nodeMailer = require('nodemailer');
+
+const APP_PASSWORD = 'fvyy tqlr bkve ukzp';
+
+const mailConfig = {
+  pool: true,
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Use SSL
+  auth: {
+    user: 'kshop9175@gmail.com',
+    pass: APP_PASSWORD,
+  },
+  authMethod: 'LOGIN', // Specify the authentication method
+};
+
+const sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport(mailConfig);
+
+  const mailOptions = {
+    from: '3KSHOP <no-reply>',
+    to: options.to,
+    subject: options.subject,
+    html: options.message,
+  };
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
